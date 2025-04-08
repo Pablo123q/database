05b1aa7565431a4d0b5b5a3136ae2a03644a246a
Pawel Szymanski <32243964+Pablo123q@users.noreply.github.com>
2025-04-08 11:16:39 +0200

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,26 +1,16 @@
-CREATE DATABASE Biblioteka_new_in_home
-ON
-PRIMARY
-(
-    NAME = 'Biblioteka_new_in_home',
-    FILENAME = 'C:\DB\Biblioteka_new_in_home.mdf',
-    SIZE = 10MB,
-    MAXSIZE = 20MB,
-    FILEGROWTH = 2MB
+
+USE master
+IF NOT EXISTS(
+select * from [sys].[databases] WHERE name = 'Biblioteka_new2'
 )
-LOG ON
-(
-    NAME = 'Biblioteka_new_in_home_log',
-    FILENAME = 'C:\DB\Biblioteka_new_in_home_log.ldf',
-    SIZE = 10MB,
-    MAXSIZE = 20MB,
-    FILEGROWTH = 2MB
-)
+CREATE DATABASE Biblioteka_new2;
+GO
+USE Biblioteka_new2;
 
 
-USE Biblioteka_new;
 
 
+--IF NOT EXISTS
 CREATE TABLE dbo.books (
     id INT IDENTITY(1,1) PRIMARY KEY,
     title NVARCHAR(200) NOT NULL,
@@ -28,8 +18,10 @@ CREATE TABLE dbo.books (
     publisher NVARCHAR(100) NULL,
     publicationYear INT NOT NULL,
     isbn NVARCHAR(20) UNIQUE,
-    status BIT NOT NULL,
-    audit_id INT NULL
+    statuss BIT NOT NULL,
+    audit_id INT NULL,
+	status_id INT NOT NULL,
+	FOREIGN KEY (status_id) REFERENCES [status](status_id)
 )
 
 CREATE TABLE dbo.authors (
@@ -63,15 +55,20 @@ CREATE TABLE dbo.users (
     email NVARCHAR(250) NOT NULL UNIQUE,
     userType NVARCHAR(20) NULL,
 	rola VARCHAR(20) CHECK (rola in('admin','librarian','user')),
+	status_id INT NOT NULL,
+	FOREIGN KEY (status_id) REFERENCES [status](status_id)
 )
 
 
 CREATE TABLE dbo.customers (
     id INT IDENTITY(1,1) PRIMARY KEY,
+	user_id INT NOT NULL,
     fullName NVARCHAR(100) NOT NULL,
     email NVARCHAR(200) NOT NULL UNIQUE,
     birthDate DATE NULL,
-	
+	FOREIGN KEY(user_id) REFERENCES dbo.users(id),
+	status_id INT NOT NULL,
+	FOREIGN KEY (status_id) REFERENCES [status](status_id)
 )
 
 
@@ -96,20 +93,47 @@ CREATE TABLE dbo.reservations (
     bookID INT NOT NULL,
     customerID INT NOT NULL,
     reservationDate DATE NOT NULL,
-    status NVARCHAR(20) NOT NULL, 
+    res_status NVARCHAR(20) NOT NULL, 
     CONSTRAINT FK_reservations_books FOREIGN KEY (bookID) REFERENCES dbo.books(id),
     CONSTRAINT FK_reservations_customers FOREIGN KEY (customerID) REFERENCES dbo.customers(id)
 )
+
+CREATE INDEX IX_reservations_book_is ON dbo.reservations(bookID)
 
 CREATE TABLE dbo.penalties (
     id INT IDENTITY(1,1) PRIMARY KEY,
     customerID INT NOT NULL,
     amount DECIMAL(10,2) NOT NULL,
     reason NVARCHAR(255) NOT NULL,
-    status NVARCHAR(20) NOT NULL,
-    CONSTRAINT FK_penalties_customers FOREIGN KEY (customerID) REFERENCES dbo.customers(id)
+	bookID INT NOT NULL,
+    status_id INT NOT NULL,
+	--[status] NVARCHAR(20) CHECK ([status] in ('naliczona','oplacona','umorzona')) DEFAULT 'naliczona', 
+    CONSTRAINT FK_penalties_customers FOREIGN KEY (customerID) REFERENCES dbo.customers(id),
+	FOREIGN KEY (bookID) REFERENCES dbo.books(id),
+	FOREIGN KEY (status_id) REFERENCES [status](status_id),
 )
 
+create table [status](
+	status_id int identity(1,1) PRIMARY KEY,
+	status_name varchar(50) NOT NULL,
+	status_desc varchar(200),
+	status_type varchar(50),--typ statusu
+	[status] BIT,
+)
 
 SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_CATALOG = 'Biblioteka_new';
 
+
+
+--INSERT INTO nazwa (kolumna) VALUES (wartosc)
+
+INSERT INTO dbo.[status]([status_name],[status_desc],[status_type],[status])
+VALUES('Dostepna','Ksiazaka w bibliotece','books',1)
+
+INSERT INTO dbo.[status]([status_name],[status_type],[status])
+VALUES('Niedostepna','books',1),
+('Aktywny','users',1),
+('Nieaktywny','users',1)
+
+
+SELECT * FROM dbo.status
